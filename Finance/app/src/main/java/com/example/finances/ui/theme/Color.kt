97fe63716f43a6cafa3f6853ad3802cd34937eca
juwumi <taisya-alekseenko@mail.ru>
2024-05-24 22:05:18 +0300

    first commit

---
+++ Finance/app/src/main/java/com/example/finances/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.finances.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue1 = Color(0xFF96C0FF)
+val Blue2 = Color(0xFF3F82E7)
+val Blue3 = Color(0xFF1357BD)
+val IncomeColor = Color(0xFF417E41)
+val ExpenseColor = Color(0xFFCC0000)
---
+++ Finance/app/src/main/java/com/example/finances/ui/theme/Theme.kt
@@ -0,0 +1,22 @@
+package com.example.finances.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColorScheme = lightColorScheme(
+    primary = Blue3,
+    secondary = Blue3,
+    tertiary = Blue3
+)
+
+@Composable
+fun FinancesTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = LightColorScheme,
+        typography = Typography,
+        content = content
+    )
+}
